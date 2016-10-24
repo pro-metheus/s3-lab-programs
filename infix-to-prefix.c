2db9a42cc8d82757c03fc371d88c39a28d8ddff2
pro-metheus <ajayajith19@gmail.com>
2016-10-24 22:04:09 +0530

    Add files via upload

---
+++ infix-to-prefix.c
@@ -0,0 +1,126 @@
+
+
+int ispref(char c){
+	if(c=='^'){
+		return 3;
+	}
+	else if(c=='*'||c=='/'){
+		return 2;
+	}
+	else if(c=='+'||c=='-'){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+	
+}
+
+int istype(char c){
+	if(c=='^'||c=='+'||c=='-'||c=='*'||c=='/'){
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+	
+//  ispref(), istype()
+char inf[100],stack[100],out[100],rev[100];
+int stop=-1;
+int otop=-1;
+int n,i;
+printf("\n Enter the number of elements :");
+scanf("%d",&n);
+printf("\n Enter the infix expression :");
+i=0;
+while(i<n){
+	scanf("%s",&inf[i]);
+	i++;
+}
+
+i=n-1;
+int j=0;
+for(i=n-1;i>=0;i--){
+	rev[j]=inf[i];
+	j++;
+}
+
+printf("\n the reversed string is expression is:");
+for(i=0;i<=n;i++){
+	printf("%c",rev[i]);
+}
+
+printf("\n\n\n");
+
+	
+char scan;
+for(i=0;i<n;i++){
+	scan=rev[i];
+	if(scan==')'){
+		stop++;
+		stack[stop]=scan;
+	}
+	else if(scan=='('){
+		while(stack[stop]!=')'){
+			otop++;
+			out[otop]=stack[stop];
+			stop--;
+		}
+		stop--;
+	}
+	else{
+		
+	if(ispref(scan)==0){
+		otop++;
+		out[otop]=scan;
+	}//operands pushed...
+	else if(stop!=-1){
+	
+    if((ispref(scan)>=ispref(stack[stop]))){
+		stop++;
+		stack[stop]=scan;
+	}//higher priority pushed
+	
+	
+	else if(ispref(scan)<ispref(stack[stop])){
+		while(ispref(stack[stop])>ispref(ispref(scan))){
+			otop++;
+			out[otop]=stack[stop];
+			stop--;
+			if(stop==-1){
+				break;
+			}
+		}
+		stop++;
+		stack[stop]=scan;
+	}}
+	else{
+		stop++;
+		stack[stop]=scan;
+	}
+	
+	
+}}
+//printf("%c",stack[0]);
+printf("\n\n");
+
+while(stop!=-1){
+	otop++;
+	out[otop]=stack[stop];
+	stop--;
+}
+//printf("%d",otop);
+/*for(i=0;i<otop;i++){
+	printf("%c",out[i]);
+}  */
+
+printf("%s",strrev(out));
+	
+}
---
+++ prefix--evaluation-final.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<math.h>
+
+bool isoperator(char c){
+	if(c=='^'||c=='*'||c=='/'||c=='+'||c=='-'){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+int operation(int x,char c, int y){
+	if(c=='^'){
+		return pow(x,y);
+	}
+	else if(c=='+'){
+		return x+y;
+		
+	}
+	else if(c=='-'){
+		return x-y;
+	}
+	else if(c=='*'){
+		return x*y;
+	}
+	else if(c=='/'){
+		return x/y;
+	}
+}
+
+int main()
+{
+	char out[100],rev[100];
+	int otop;
+	int n,i,var1=0,var2=0,var3=0,rtop=-1,j=0;
+	printf("enter number of terms :");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%s",&rev[i]);
+	}
+	i=n-1;
+	for(i=n-1;i>=0;i++){
+		out[j]=rev[i];
+		j++;
+	}
+	for()
+	int res[100];
+	otop=0;
+	while(otop<n){
+		if(isoperator(out[otop])==false){
+			rtop++;
+			res[rtop]=out[otop];
+			otop++;
+		}
+		else{printf("top is now %c",res[rtop]);
+			var1=res[rtop]-'0';
+			printf("\n var 1 =%d",var1);
+			rtop--;
+			var2=res[rtop]-'0';
+				printf("\n var 2 =%d",var2);
+			rtop--;
+			var3=operation(var2,out[otop],var1);
+				printf("\n var3 is %d",var3);
+			otop++;
+			rtop++;
+			res[rtop]=var3;
+			
+		}
+	}
+	printf("\n\n\n\n");
+	printf("%d",res[rtop]);
+	
+}
